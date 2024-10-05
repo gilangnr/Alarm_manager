3fc4ce93831bbbff229d5c6d7d8bfc9c5a2c2631
Gilang <gilangnurrizki173@gmail.com>
2024-10-05 20:58:05 +0700

    implement in activity

--- app/src/main/java/com/example/alarmmanager/MainActivity.kt
+++ app/src/main/java/com/example/alarmmanager/MainActivity.kt
@@ -18,21 +18,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
     private lateinit var alarmReceiver: AlarmReceiver
 
     //    permission
-    private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-        if (isGranted) {
-            Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
-        }
-    }
-    companion object {
-        private const val DATE_PICKER_TAG = "DatePicker"
-        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
-        private const val TIMRE_PICKER_REPEAT_TAG = "TimePickerRepat"
-    }
+//    private val requestPermissionLauncher =
+//        registerForActivityResult(
+//            ActivityResultContracts.RequestPermission()
+//        ) { isGranted: Boolean ->
+//        if (isGranted) {
+//            Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+//        } else {
+//            Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,14 +37,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
         setContentView(binding?.root)
 
 //        permission
-        if (Build.VERSION.SDK_INT >= 33) {
-            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-        }
+//        if (Build.VERSION.SDK_INT >= 33) {
+//            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+//        }
 
 //        LISTENER ONE TIME ALARM
         binding?.btnOnceDate?.setOnClickListener(this)
         binding?.btnOnceTime?.setOnClickListener(this)
         binding?.btnSetOnceAlarm?.setOnClickListener(this)
+
+//       listener repeating alarm
+        binding?.btnRepeatingTime?.setOnClickListener(this)
+        binding?.btnSetRepeatingAlarm?.setOnClickListener(this)
 
         alarmReceiver = AlarmReceiver()
     }
@@ -73,6 +73,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
                     onceTime,
                     onceMessage)
             }
+            R.id.btn_repeating_time -> {
+                val timePickerFragmentRepeat = TimePickerFragment()
+                timePickerFragmentRepeat.show(supportFragmentManager, TIME_PICKER_REPEAT_TAG)
+            }
+            R.id.btn_set_repeating_alarm -> {
+                val repeatTime = binding?.tvRepeatingTime?.text.toString()
+                val repeatMessage = binding?.edtRepeatingMessage?.text.toString()
+                alarmReceiver.setRepeatingAlarm(this, AlarmReceiver.TYPE_REPEATING, repeatTime, repeatMessage)
+            }
         }
     }
 
@@ -93,7 +102,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
 
         when (tag) {
             TIME_PICKER_ONCE_TAG -> binding?.tvOnceTime?.text = dateFormat.format(calendar.time)
-            TIMRE_PICKER_REPEAT_TAG -> {}
+            TIME_PICKER_REPEAT_TAG -> { binding?.tvRepeatingTime?.text = dateFormat.format(calendar.time)}
             else -> {
 
             }
@@ -107,5 +116,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, DatePickerFragme
         binding = null
     }
 
+
+    companion object {
+        private const val DATE_PICKER_TAG = "DatePicker"
+        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
+        private const val TIME_PICKER_REPEAT_TAG = "TimePickerRepat"
+    }
 
 }
